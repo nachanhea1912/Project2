e6703d070a035bda14bc4c5c5e646bb84f93e9b5
nachanhea1912 <cksgo7343@naver.com>
2024-03-14 12:03:11 +0900

    안드로이드 를 활용하여 중고 거래 가는 앱 제작

---
+++ plumMarketSpring/src/main/java/com/bitc/plummarketdb/PlumMarketDbApplication.java
@@ -0,0 +1,12 @@
+package com.bitc.plummarketdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumMarketDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlumMarketDbApplication.class, args);
+    }
+}
